0cc528e64e13c8d4f669c09a7366031a77df525b
Leyza <dragonzgirl11@gmail.com>
2022-04-09 17:53:10 -0400

    Changed diagram creation functionality to allow creation and modification for both before or after calling makeDiagram. Polished diagram and sidebar style more

--- pub/js/example.js
+++ pub/js/example.js
@@ -1,20 +1,19 @@
 "use strict"
 
-const newdiagram = new LayersDiagram("testdiagram", "40vw", "10vw", "50vw");
+const newdiagram = new LayersDiagram("testdiagram", "30vw", "10vw", "30vw");
 newdiagram.makeDiagram(document.getElementById("fordiagram"));
 
-//newdiagram.addComponent(true, "tbun", "images/bottom_bun.png");
-newdiagram.addComponent(true, "tbun", "images/top_bun.png");
-newdiagram.addComponent(true, "bbun", "images/bottom_bun.png", 60);
+newdiagram.addComponent(true, "top bun", "images/top_bun.png");
+newdiagram.addComponent(true, "bottom bun", "images/bottom_bun.png", 60);
 newdiagram.addComponent(true, "patty", "images/patty.png", 60);
 newdiagram.addComponent(true, "lettuce", "images/lettuce.png", 75);
 newdiagram.addComponent(true, "tomato", "images/tomato.png", 70);
 newdiagram.addComponent(true, "onions", "images/onions.png", 80);
 
-newdiagram.addLayer("bbun");
+newdiagram.addLayer("bottom bun");
 newdiagram.addLayer("patty");
 newdiagram.addLayer("lettuce");
-newdiagram.addLayer("tbun");
+newdiagram.addLayer("top bun");
 
 newdiagram.addComponentDescription("patty", "A very juicy 100% premium beef patty with 20% fat and 80% lean and smoked to perfection then charred on the grill.");
 newdiagram.addComponentDescription("tbun", "Pillowy bun sprinkled with sesame seeds.");
--- pub/js/layers.js
+++ pub/js/layers.js
@@ -4,11 +4,13 @@ function LayersDiagram(id, diagramwidth, sidebarwidth, sidebarheight, allowEdit 
     this.diagramwidth = diagramwidth;
     this.sidebarwidth = sidebarwidth;
     this.sidebarheight = sidebarheight;
+    this.madeDiagram = false;
 
     this.layers = 0;
-    this.layersOrder = [];
-    this.components = {};
+    this.layersOrder = [null];
+    this.components = { "_dummy": { image: "images/bottom_bun.png", description: null, overlap: 0 }};
     this.allowEdit = allowEdit;
+    this.sidebarComponents = 0;
     this.sidebar = [];
 }
 
@@ -24,19 +26,33 @@ LayersDiagram.prototype = {
             layersdiagram.style = `width: calc(${this.diagramwidth} + ${this.sidebarwidth}); min-height: ${this.sidebarheight}; margin: auto; border-radius: 5%; background-color: white; position: relative;`;
 
             const sidebar = document.createElement("div");
-            sidebar.style = `width: calc(${this.sidebarwidth}); max-height: ${this.sidebarheight}; float: left; margin: auto; position: relative; background-color: slategray; overflow-y: auto; overflow-x: hidden; padding-right: 15px;`
-            sidebar.className = "sidebar";
+            sidebar.style = `width: ${this.sidebarwidth}; max-height: ${this.sidebarheight}; float: left; position: relative; display: flex; flex-direction: column; justify-content: space-between;`
+            sidebar.className = "sidebarbox";
+
+            const sidebarComponentHolder = document.createElement("div");
+            sidebarComponentHolder.style = `width: calc(100% - 0.5vw); min-height: 0; background-color: lightgrey; overflow-y: auto; overflow-x: hidden;`;
+            sidebarComponentHolder.className = "sidebar";
+
             layersdiagram.appendChild(sidebar);
+            sidebar.appendChild(sidebarComponentHolder);
         }
         else {
             layersdiagram.style = `width: ${this.diagramwidth}; min-height: ${this.sidebarheight}; margin: 10vw; border-radius: 5%; background-color: white; position: relative;`;
         }
 
         const diagram = document.createElement("div");
-        diagram.style = `width: ${this.diagramwidth}; margin-left: ${this.sidebarwidth}; margin-top: 2.5%; margin-bottom: 2.5%; 
-                        background-color: lightgray; position: relative; top: 50%; transform: translateY(0%);`
-        diagram.className = "diagram"
+        diagram.style = `width: ${this.diagramwidth}; margin-left: ${this.sidebarwidth}; border: 0.5vw solid black; border-radius: 5vw;`;
+        diagram.className = "diagrambox";
         layersdiagram.appendChild(diagram);
+
+        const componentHolder = document.createElement("div");
+        componentHolder.style = `width: 90%; margin: 5%; position: relative; top: 50%; transform: translateY(0%);`;
+        componentHolder.className = "diagram";
+        diagram.appendChild(componentHolder);
+
+        //events
+        componentHolder.onmouseover = () => { this.fanout() };
+        componentHolder.onmouseout = () => { this.collapse() };
 
         if (element == null) {
             const body = $("body");
@@ -45,8 +61,6 @@ LayersDiagram.prototype = {
         else {
             element.append(layersdiagram);
         }
-
-        this.addLayer(null);
 
         // Add popup window
         const popup = document.createElement("div");
@@ -70,61 +84,87 @@ LayersDiagram.prototype = {
         //events
         exitbutton.onclick = () => this.popupHide();
 
+        this.madeDiagram = true;
+        this.updateAllSidebar();
+        this.updateAllLayers();
     },
 
     addComponent: function (putInSidebar, name, image, overlap = 50) {
         // Create a new component
-
         let update = false;
         if (name in this.components) {
             update = true;
         }
 
-        this.components[name] = { image: image, nutrition: null, price: null, description: null, overlap: overlap };
+        this.components[name] = { image: image, description: null, overlap: overlap };
         if (putInSidebar) {
             if (!update) {
                 this.sidebar.push(name);
-                this.makeSidebarComponent(name);
             }
-            else {
-                this.updateSidebarComponent(name);
+
+            if (this.madeDiagram) {
+                this.updateAllSidebar();
             }
         }
     },
 
-    makeSidebarComponent: function (componentName) {
+    addSidebarComponent: function () {
         // Make a component icon in sidebar
 
         if (this.allowEdit) {
             const sidebar = $(`#${this.id} .sidebar`);
             const sidebarcomponent = document.createElement("div");
-            sidebarcomponent.id = componentName;
             sidebarcomponent.className = "sidebarcomponent";
-            sidebarcomponent.style = `width: calc(${this.sidebarwidth} - 4px); height: calc(${this.sidebarwidth} - 4px); border: 2px solid black;`;
             sidebarcomponent.draggable = true;
 
             // events
+            sidebarcomponent.onclick = (e) => { this.popupShow(e) };
             sidebarcomponent.addEventListener('dragstart', (e) => { e.dataTransfer.setData('text', JSON.stringify(["new", e.target.id])) });
             sidebarcomponent.addEventListener("dragover", function (e) { e.preventDefault() });
             sidebarcomponent.ondrop = (e) => this.dropComponentInSidebar(e);
 
             const image = document.createElement("img");
-            image.src = this.components[componentName].image;
-            image.style = "max-width: 100%; max-height: 100%; transform: translateY(50%);";
+            image.style = `padding-top: 1vw; padding-bottom: 1vw; margin: auto; max-width: 95%; display: block; position: relative;`;
             image.draggable = false;
+
+            const tooltip = document.createElement("span");
+            tooltip.className = "sidebartooltiptext";
 
             sidebar.append(sidebarcomponent);
             sidebarcomponent.append(image);
+            sidebarcomponent.append(tooltip);
+
+            this.sidebarComponents++;
         }
     },
 
-    updateSidebarComponent: function (componentName) {
-        // Update sidebar component image
-
-        $(`#${this.id} #${componentName}`).children()[0].src = this.components[componentName].image;
+    removeFirstSidebarComponent: function () {
+        $(`#${this.id} .sidebar`).children()[0].remove();
+        this.sidebarComponents--;
     },
 
-    addComponentNutritionalInfo: function (componentName, calories=null, protein=null, satFat=null, totFat=null, sugars=null, totalCarb=null, fibre=null, sodium=null, other=null) {
+    updateAllSidebar: function () {
+        if (this.sidebarComponents > this.sidebar.length) {
+            const iter = this.sidebarComponents - this.sidebar.length;
+            for (let i = 0; i < iter; ++i) {
+                this.removeFirstSidebarComponent();
+            }
+
+        } else if (this.sidebarComponents < this.sidebar.length) {
+            const iter = this.sidebar.length - this.sidebarComponents;
+            for (let i = 0; i < iter; ++i) {
+                this.addSidebarComponent();
+            }
+        }
+
+        $(`#${this.id} .sidebar`).children().each((i, c) => {
+            c.id = this.sidebar[i];
+            c.children[0].src = this.components[this.sidebar[i]].image;
+            c.children[1].innerText = this.sidebar[i];
+        })
+    },
+
+    /*addComponentNutritionalInfo: function (componentName, calories=null, protein=null, satFat=null, totFat=null, sugars=null, totalCarb=null, fibre=null, sodium=null, other=null) {
         // Add nutritional info to a component
 
         if (componentName in this.components) {
@@ -141,7 +181,7 @@ LayersDiagram.prototype = {
         if (componentName in this.components) {
             this.components[componentName].price = price;
         }
-    },
+    },*/
 
     addComponentDescription: function (componentName, description) {
         // Add a description to a component
@@ -151,53 +191,53 @@ LayersDiagram.prototype = {
         }
     },
 
-    addLayer: function (componentName=null) {
+    addLayer: function (componentName) {
+        // Add layer to layersOrder tracking and update diagram is applicable
+
+        if (componentName in this.components) {
+            this.layersOrder.push(componentName);
+        }
+
+        if (this.madeDiagram) {
+            this.updateAllLayers();
+        }
+    },
+
+    addDiagramLayer: function (isDummy) {
         // Add a layer to the diagram
 
-        if (componentName == null || componentName in this.components) {
-            const diagram = $(`#${this.id} .diagram`);
-            const newlayer = document.createElement("div");
-            newlayer.className = "layer";
-            newlayer.style = `margin-left: 5%; z-index: ${this.layers + 1}; position: relative;`;
+        const diagram = $(`#${this.id} .diagram`);
+        const newlayer = document.createElement("div");
+        newlayer.className = "layer";
+        newlayer.style = `width: 100%; z-index: ${this.layers + 1}; position: relative; transition: transform 0.5s ease;`;
 
-            // events
-            if (componentName != null) {
-                newlayer.draggable = true;
-                newlayer.onmouseover = () => { this.fanout() };
-                newlayer.onmouseout = () => { this.collapse() };
-                newlayer.onclick = (e) => { this.popupShow(e) };
-                newlayer.addEventListener('dragstart', (e) => { e.dataTransfer.setData('text', JSON.stringify(["layer", e.target.style.zIndex])) });
-            }
-
-            const image = document.createElement("img");
-            image.className = "layerimage";
-
-            if (!(componentName == null)) {
-                image.src = this.components[componentName].image;
-            }
-            else {
-                image.src = "images/bottom_bun.png";
-                image.style = "opacity: 0";
-                newlayer.style += `margin-left: 5%; z-index: ${this.layers + 1}; position: relative; width: 100%; height: 8vw;`;
-            }
-
-            //events
-            image.ondragenter = (e) => { this.dragComponent(e) };
-            image.ondragleave = () => { this.dragLeaveComponent() };
-            image.addEventListener("dragover", function (e) { e.preventDefault() });
-            image.ondrop = (e) => { this.dropComponent(e) };
-            image.draggable = false;
-
-            newlayer.append(image);
-            diagram.prepend(newlayer);
-
-            this.layersOrder.push(componentName);
-            this.layers++;
-
-            setTimeout(() => {
-                this.collapse();
-            }, 300);
+        // events
+        if (!isDummy) {
+            newlayer.draggable = true;
+            //newlayer.onmouseover = () => { this.fanout() };
+            //newlayer.onmouseout = () => { this.collapse() };
+            newlayer.addEventListener('dragstart', (e) => { e.dataTransfer.setData('text', JSON.stringify(["layer", e.target.style.zIndex])) });
         }
+
+        const image = document.createElement("img");
+        image.className = "layerimage";
+        image.style = `width: 100%;`;
+
+        //events
+        image.onclick = (e) => { this.popupShow(e) };
+        image.ondragenter = (e) => { this.dragComponent(e); e.target.style.backgroundColor = 'rgba(255, 255, 255, 0.5)' };
+        image.ondragleave = (e) => { this.dragLeaveComponent(); e.target.style.backgroundColor = 'rgba(255, 255, 255, 0)' };
+        image.addEventListener("dragover", function (e) { e.preventDefault() });
+        image.ondrop = (e) => { this.dropComponent(e); e.target.style.backgroundColor = 'rgba(255, 255, 255, 0)' };
+        image.draggable = false;
+
+        newlayer.append(image);
+        diagram.prepend(newlayer);
+        this.layers++;
+
+        setTimeout(() => {
+            this.collapse();
+        }, 300);
     },
 
     removeTopLayer: function () {
@@ -205,8 +245,8 @@ LayersDiagram.prototype = {
         this.layers -= 1;
     },
 
-    collapse: function (ignoreIndex=-1) {
-        // Compress the layers in the diagram
+    /*collapse: function (ignoreIndex=-1) {
+        // Compress the layers in the diagram to the middle
 
         let greaterOverlap = 0;
         let lesserOverlap = 0;
@@ -239,6 +279,29 @@ LayersDiagram.prototype = {
         if (this.layers > 1) {
             layers[this.layers - 1].style.transform = `translateY(${greaterOverlap}px)`
         }
+    },*/
+
+    collapse: function (ignoreIndex = -1) {
+        // Compress the layers in the diagram to the top
+
+        let overlap = 0;
+
+        const layers = $(`#${this.id} .diagram`).children();
+
+        for (i = this.layers - 2; i > 0; --i) {
+            let overlapPercent = 0;
+            if (i != ignoreIndex) {
+                overlapPercent = this.components[this.layersOrder[i]].overlap;
+            }
+
+            overlap -= layers[this.layers - i - 1].offsetHeight * overlapPercent / 100.0;
+
+            layers[this.layers - i - 1].style.transform = `translateY(${overlap}px)`;
+        }
+
+        if (this.layers > 1) {
+            layers[this.layers - 1].style.transform = `translateY(${overlap}px)`
+        }
     },
 
     fanout: function () {
@@ -264,7 +327,8 @@ LayersDiagram.prototype = {
         // Move diagram to show dragged component location
 
         const index = e.target.parentElement.style.zIndex;
-        this.collapse((index > Math.ceil(this.layers / 2)) ? index : index - 1);
+        //this.collapse((index > Math.ceil(this.layers / 2)) ? index : index - 1); // For when fan out from the middle
+        this.collapse(index - 1); // For when fan out from the top
     },
 
     dragLeaveComponent: function (e) {
@@ -297,13 +361,16 @@ LayersDiagram.prototype = {
             const index1 = parseInt(droploc.parentElement.style.zIndex) - 1;
             const index2 = parseInt(data[1]) - 1;
 
-            const component = this.layersOrder[index2];
-            this.layersOrder.splice(index2, 1);
-            if (index1 > index2) {
-                this.layersOrder.splice(index1, 0, component);
-            }
-            else {
-                this.layersOrder.splice(index1 + 1, 0, component);
+            if (index1 != index2) {
+                const component = this.layersOrder[index2];
+                this.layersOrder.splice(index2, 1);
+
+                if (index1 > index2) {
+                    this.layersOrder.splice(index1, 0, component);
+                }
+                else {
+                    this.layersOrder.splice(index1 + 1, 0, component);
+                }
             }
             
         }
@@ -330,28 +397,59 @@ LayersDiagram.prototype = {
     },
 
     updateAllLayers: function () {
+        if (this.layers > this.layersOrder.length) {
+            const iter = this.layers - this.layersOrder.length;
+            for (let i = 0; i < iter; ++i) {
+                this.removeTopLayer();
+            }
+            console.log(this.layers == this.layersOrder.length);
+
+        } else if (this.layers < this.layersOrder.length) {
+            const iter = this.layersOrder.length - this.layers;
+            for (let i = 0; i < iter; ++i) {
+                if (this.layers == 0) {
+                    this.addDiagramLayer(true);
+                } else {
+                    this.addDiagramLayer(false);
+                }
+            }
+            console.log(this.layers == this.layersOrder.length);
+        }
+
         let index = 0;
         $(`#${this.id} .diagram`).children().each((i, c) => {
             if (this.layersOrder[this.layers - index - 1] != null) {
                 c.children[0].src = this.components[this.layersOrder[this.layers - index - 1]].image;
+            } else {
+                c.children[0].src = this.components["_dummy"].image;
+                c.style.opacity = 0;
             }
             index += 1;
         });
     },
 
     popupShow: function (e) {
-        document.getElementById("popup").className = "show";
-
         let obj = e.target;
-        if (obj.className != "layer" && obj.parentElement.className == "layer") {
+        if ((obj.className != "layer" && obj.parentElement.className == "layer") || (obj.className != "sidebarcomponent" && obj.parentElement.className == "sidebarcomponent")) {
             obj = obj.parentElement;
         }
 
         if (obj.className == "layer") {
+            document.getElementById("popup").className = "show";
+
             if (this.components[this.layersOrder[obj.style.zIndex - 1]].description != null && this.components[this.layersOrder[obj.style.zIndex - 1]].description != "") {
                 $("#popup .popupcontent")[0].innerText = this.components[this.layersOrder[obj.style.zIndex - 1]].description;
             }
             else {
+                $("#popup .popupcontent")[0].innerText = "There is no description.";
+            }
+
+        } else if (obj.className == "sidebarcomponent") {
+            document.getElementById("popup").className = "show";
+
+            if (this.components[obj.id].description != null && this.components[obj.id].description != "") {
+                $("#popup .popupcontent")[0].innerText = this.components[obj.id].description;
+            } else {
                 $("#popup .popupcontent")[0].innerText = "There is no description.";
             }
         }
